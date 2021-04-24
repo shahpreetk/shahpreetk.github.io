b83b8b58327da9155bddfb79453b244a6c4328a1
Preet Shah <shahpreetk@gmail.com>
2021-04-24 20:14:53 +0530

    setting up pages and design of site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Sidebar from "./components/Sidebar";
+import Extracurriculars from "./pages/Extracurriculars";
+import Experience from "./pages/Experience";
+import Skills from "./pages/Skills";
 
 function App() {
   return (
     <>
-      <div className="bg-white font-body text-lg">
-        <h1>Portfolio Preet Shah</h1>
-        <hr />
-        <p>In Progress...</p>
+      <div
+        data-todo-x-data="{ open: false }"
+        data-todo-at-keydown-window-escape="open = false"
+        className="h-screen flex overflow-hidden bg-white"
+      >
+        <Sidebar />
+        <Switch>
+          <Route path="/extracurriculars" component={Extracurriculars} exact />
+          <Route path="/experience" component={Experience} exact />
+          <Route path="/skills" component={Skills} exact />
+          <Route path={"/" | "/about"} component={About} exact />
+        </Switch>
       </div>
     </>
   );
