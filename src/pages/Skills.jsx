c9bcbfcb12599a38ff2ab5da339ff014509c4a2d
Preet Shah <shahpreetk@gmail.com>
2021-04-25 17:52:00 +0530

    layout for skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -26,32 +26,215 @@ const Skills = () => {
                     </div>
                   </div>
 
-                  <div className="py-3 xl:pt-6 xl:pb-0">
-                    <h2 className="sr-only">Description</h2>
-                    <div className="prose max-w-none">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Expedita, hic? Commodi cumque similique id tempora
-                        molestiae deserunt at suscipit, dolor voluptatem,
-                        numquam, harum consequatur laboriosam voluptas tempore
-                        aut voluptatum alias?
-                      </p>
-                      <ul>
-                        <li>
-                          Tempor ultrices proin nunc fames nunc ut auctor vitae
-                          sed. Eget massa parturient vulputate fermentum id
-                          facilisis nam pharetra. Aliquet leo tellus.
-                        </li>
-                        <li>
-                          Turpis ac nunc adipiscing adipiscing metus tincidunt
-                          senectus tellus.
-                        </li>
-                        <li>
-                          Semper interdum porta sit tincidunt. Dui suspendisse
-                          scelerisque amet metus eget sed. Ut tellus in sed
-                          dignissim.
-                        </li>
-                      </ul>
+                  <div className="px-4 flex justify-center sm:px-6 lg:px-8 py-12">
+                    <div className="min-w-0">
+                      <nav aria-label="Progress">
+                        <ol className="overflow-hidden">
+                          <li className="relative pb-10">
+                            <div
+                              className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
+                              aria-hidden="true"
+                            ></div>
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              data-todo-x-description="Complete Step"
+                            >
+                              <span className="h-9 flex items-center">
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
+                                  <svg
+                                    className="w-5 h-5 text-white"
+                                    data-todo-x-description="Heroicon name: solid/check"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase">
+                                  HTML & CSS
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Vitae sed mi luctus laoreet.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+
+                          <li className="relative pb-10">
+                            <div
+                              className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
+                              aria-hidden="true"
+                            ></div>
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              data-todo-x-description="Complete Step"
+                            >
+                              <span className="h-9 flex items-center">
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
+                                  <svg
+                                    className="w-5 h-5 text-white"
+                                    data-todo-x-description="Heroicon name: solid/check"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase">
+                                  JavaScript
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Vitae sed mi luctus laoreet.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+
+                          <li className="relative pb-10">
+                            <div
+                              className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
+                              aria-hidden="true"
+                            ></div>
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              data-todo-x-description="Complete Step"
+                            >
+                              <span className="h-9 flex items-center">
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
+                                  <svg
+                                    className="w-5 h-5 text-white"
+                                    data-todo-x-description="Heroicon name: solid/check"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase">
+                                  React
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Vitae sed mi luctus laoreet.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+
+                          <li className="relative pb-10">
+                            <div
+                              className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
+                              aria-hidden="true"
+                            ></div>
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              aria-current="step"
+                              data-todo-x-description="Current Step"
+                            >
+                              <span
+                                className="h-9 flex items-center"
+                                aria-hidden="true"
+                              >
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
+                                  <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full"></span>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase text-indigo-600">
+                                  NodeJS
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Cursus semper viverra facilisis et et some
+                                  more.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+
+                          <li className="relative pb-10">
+                            <div
+                              className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
+                              aria-hidden="true"
+                            ></div>
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              aria-current="step"
+                              data-todo-x-description="Current Step"
+                            >
+                              <span
+                                className="h-9 flex items-center"
+                                aria-hidden="true"
+                              >
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
+                                  <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full"></span>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase text-indigo-600">
+                                  MongoDB
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Cursus semper viverra facilisis et et some
+                                  more.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+
+                          <li className="relative ">
+                            <a
+                              href="/"
+                              className="relative flex items-start group"
+                              data-todo-x-description="Upcoming Step"
+                            >
+                              <span
+                                className="h-9 flex items-center"
+                                aria-hidden="true"
+                              >
+                                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
+                                  <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"></span>
+                                </span>
+                              </span>
+                              <span className="ml-4 min-w-0 flex flex-col">
+                                <span className="text-xs font-semibold tracking-wide uppercase text-gray-500">
+                                  React Native
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                  Iusto et officia maiores porro ad non quas.
+                                </span>
+                              </span>
+                            </a>
+                          </li>
+                        </ol>
+                      </nav>
                     </div>
                   </div>
                 </div>
