09939ed6db57d0f0103cd67d2d2c9f0cb39f64fe
Preet Shah <shahpreetk@gmail.com>
2021-04-25 19:23:58 +0530

    hashrouter try again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
--- src/pages/Extracurriculars.jsx
+++ src/pages/Extracurriculars.jsx
@@ -49,18 +49,18 @@ const Extracurriculars = () => {
                     </div>
                   </div>
 
-                  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="mt-10">
                       <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
                         {features.map((feature) => (
                           <div key={feature.name} className="relative">
                             <dt>
                               {" "}
-                              <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                              <p className="ml-6 text-lg leading-6 font-medium text-gray-900">
                                 {feature.name}
                               </p>
                             </dt>
-                            <dd className="mt-2 ml-16 text-base text-gray-500">
+                            <dd className="mt-2 ml-12 text-base text-gray-500">
                               {feature.description}
                             </dd>
                           </div>
