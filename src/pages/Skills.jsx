fd57f89230ad04290682983239b2c05f04c06d7a
Preet Shah <shahpreetk@gmail.com>
2021-04-28 16:00:18 +0530

    added tools in skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -287,6 +287,23 @@ const Skills = () => {
                               clipRule="evenodd"
                             ></path>
                           </svg>
+                          <span className="text-md text-gray-600">Git</span>
+                        </li>
+                        <li className="flex space-x-3">
+                          <svg
+                            className="flex-shrink-0 h-6 w-6 text-green-500"
+                            data-todo-x-description="Heroicon name: solid/check"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
                           <span className="text-md text-gray-600">
                             Bootstrap
                           </span>
@@ -345,6 +362,57 @@ const Skills = () => {
                             ></path>
                           </svg>
                           <span className="text-md text-gray-600">Postman</span>
+                        </li>
+                        <li className="flex space-x-3">
+                          <svg
+                            className="flex-shrink-0 h-6 w-6 text-green-500"
+                            data-todo-x-description="Heroicon name: solid/check"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                          <span className="text-md text-gray-600">Netlify</span>
+                        </li>
+                        <li className="flex space-x-3">
+                          <svg
+                            className="flex-shrink-0 h-6 w-6 text-green-500"
+                            data-todo-x-description="Heroicon name: solid/check"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                          <span className="text-md text-gray-600">Heroku</span>
+                        </li>
+                        <li className="flex space-x-3">
+                          <svg
+                            className="flex-shrink-0 h-6 w-6 text-green-500"
+                            data-todo-x-description="Heroicon name: solid/check"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                          <span className="text-md text-gray-600">Surge</span>
                         </li>
                       </ul>
                     </div>
