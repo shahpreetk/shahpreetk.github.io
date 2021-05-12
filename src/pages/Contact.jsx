b6836dc8a0e3565a4371d933809c326c4abac59f
Preet Shah <shahpreetk@gmail.com>
2021-05-12 17:08:58 +0530

    contact page desktop done, mobile has issues

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,82 +2,72 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col w-0 flex-1 overflow-hidden">
-      {/* <main
-        className="flex-1 relative overflow-y-auto focus:outline-none"
-        tabIndex={-1}
-        data-todo-x-data=""
-        data-todo-x-init="$el.focus()"
-      >
-        <div className="py-12 xl:py-10">
-          <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 xl:max-w-5xl">
-            <div
-              className="xl:col-span-2 xl:pr-8
-
-             xl:border-gray-200"
-            > */}
-      <div className="bg-white pt-16 lg:py-24">
-        <div className="pb-16 bg-teal-500 lg:pb-0 lg:z-10 lg:relative">
-          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
-            <div className="relative lg:-my-8">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
-              ></div>
-              <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-                <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-                  <img
-                    className="object-cover lg:h-full lg:w-full"
-                    src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&amp;ixqx=cII20c177f&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2102&amp;q=80"
-                    alt=""
-                  />
+    <>
+      <div className="flex flex-col w-0 flex-1 overflow-y-auto">
+        <div className="bg-white pt-16 lg:py-24 min-h-screen">
+          <div className="pb-16 bg-teal-500 lg:pb-0 lg:z-10 lg:relative">
+            <div className="lg:mx-auto lg:my-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
+              <div className="relative lg:-my-8">
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
+                ></div>
+                <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
+                  <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+                    <img
+                      className="object-cover lg:h-full lg:w-full"
+                      src="/images/shahpreetk.JPG"
+                      alt="me"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
-              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
-                <blockquote>
-                  <div>
-                    <svg
-                      className="h-12 w-12 text-white opacity-25"
-                      fill="currentColor"
-                      viewBox="0 0 32 32"
-                      aria-hidden="true"
-                    >
-                      <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"></path>
-                    </svg>
-                    <p className="mt-6 text-2xl font-medium text-white">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed urna nulla vitae laoreet augue. Amet feugiat est
-                      integer dolor auctor adipiscing nunc urna, sit.
-                    </p>
-                  </div>
-                  <footer className="mt-6">
-                    <p className="text-base font-medium text-white">
-                      Email -{" "}
-                      <a
-                        href="mailto:shahpreetk@outlook.com"
-                        target="_blank"
-                        rel="noreferrer"
+              <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+                  <blockquote>
+                    <div>
+                      <svg
+                        className="h-12 w-12 text-white opacity-25"
+                        fill="currentColor"
+                        viewBox="0 0 32 32"
+                        aria-hidden="true"
                       >
-                        shahpreetk@outlook.com
-                      </a>
-                    </p>
-                    <p className="text-base font-medium text-indigo-100">
-                      CEO at PureInsights
-                    </p>
-                  </footer>
-                </blockquote>
+                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"></path>
+                      </svg>
+                      <p className="mt-6 text-2xl font-medium text-white">
+                        Want to rant about coding or discuss painting? Get in
+                        touch!
+                      </p>
+                    </div>
+                    <footer className="mt-6">
+                      <p className="text-base font-medium text-white">
+                        Email -{" "}
+                        <a
+                          href="mailto:shahpreetk@outlook.com"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          shahpreetk@outlook.com
+                        </a>
+                      </p>
+                      <p className="text-base font-medium text-indigo-100">
+                        Phone -{" "}
+                        <a
+                          href=" https:api.whatsapp.com/send?phone=+919820740551"
+                          target="_blank"
+                        >
+                          +919820740551
+                        </a>
+                      </p>
+                    </footer>
+                  </blockquote>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* </div>
-          </div>
-        </div>
-      </main> */}
-    </div>
+    </>
   );
 };
 
