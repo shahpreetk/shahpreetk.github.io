302a06d90fb025f87dbe660e80d11634b68c49a1
Preet Shah <shahpreetk@gmail.com>
2021-06-25 14:01:41 +0530

    extracurriculars data in new file

---
+++ src/data/extracurriculars.js
@@ -0,0 +1,30 @@
+export const extracurriculars = [
+  {
+    id: 1,
+    name: "IETE",
+    url: "https://www.instagram.com/iete_kjsieit/",
+    description:
+      "I was a part of the Institution of Electronics and Telecommunication Engineers(IETE) chapter of my college for 4 years. Over the years we held various workshops, bootcamps, competitions, and events like the Data Science Bootcamp, Web Development Workshop, Poster Competition, and Technical Mock Interview. In the latter 2 years I had also held the post of Treasurer and Chairperson.",
+  },
+  {
+    id: 2,
+    name: "NSS",
+    url: "https://www.instagram.com/nss__kjsieit/",
+    description:
+      "The National Service Scheme(NSS) is a Central Sector Scheme of Government of India, Ministry of Youth Affairs & Sports. It was a two years experience of partaking and later organising as the Assistant leader, various events whose sole purpose was community service. In these years I have organised multiple Blood Donation drives, organised street plays to spread Awareness on various social issues, painted Railway station walls and held Medical camps in villages.",
+  },
+  {
+    id: 3,
+    name: "LEO",
+    url: "https://www.instagram.com/leodistrict3231a1/",
+    description:
+      "LEO is the youth organisation of Lions Clubs International. It focuses on social service, fellowship and networking events. I have been a part of this organisation since the last three years and am currently the Past President of Leo Club of Walkeshwar, President of Leo District 3231 A1 and Chief Website Officer of Leo Multiple 3231, India. As the District President my role is to ensure the proper functioning of the 9 clubs laying in the South Mumbai region.",
+  },
+  {
+    id: 4,
+    name: "AURA",
+    url: "https://www.instagram.com/auralicious_/",
+    description:
+      "I was a part of the team that created the college magazine, AURA. I have been a Designer and later a Content Writer and Editor. I also love to paint and have continued to do so as a hobby over the years.",
+  },
+];
