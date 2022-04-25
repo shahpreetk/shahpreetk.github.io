9e32e783a339772324ff75c04f2b9a990a07f90b
Preet Shah <shahpreetk@gmail.com>
2022-04-25 21:59:45 +0100

    added soft serve canvas card

--- src/data/DigitalArtData.js
+++ src/data/DigitalArtData.js
@@ -2,6 +2,12 @@
 const BASE_DIGITAL_ART_URL = "/images/digitalart";
 export const DigitalArtData = [
   {
+    id: 3,
+    title: "Soft Serve Canvas Card",
+    previewImg: `${BASE_DIGITAL_ART_URL}/soft_serve_canvas_card.jpeg`,
+    instagramUrl: "https://www.instagram.com/p/CcxtZksjRm3/",
+  },
+  {
     id: 2,
     title: "Moon Outside the Window",
     previewImg: `${BASE_DIGITAL_ART_URL}/moon_outside_the_window.jpeg`,
