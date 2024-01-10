d9800d15d5ac0e4b246251012f5998fbafbbee52
Preet Shah <shahpreetk@gmail.com>
2024-01-10 23:29:58 +0000

    books read in 2023

---
+++ blog/data2023.js
@@ -0,0 +1,28 @@
+const data2023 = [
+  { id: 1, bookTitle: "The Pale Blue Eye", author: "Louis Bayard" },
+  { id: 2, bookTitle: "Wobble to Death", author: "Peter Lovesey" },
+  { id: 3, bookTitle: "Presumed Innocent", author: "Scott Turow" },
+  { id: 4, bookTitle: "The Franchise Affair", author: "Josephine Tey" },
+  { id: 5, bookTitle: "Beast in View", author: "Margaret Millar" },
+  { id: 6, bookTitle: "The Woman in Cabin 10", author: "Ruth Mare" },
+  { id: 7, bookTitle: "In the Woods", author: "Tana French" },
+  { id: 8, bookTitle: "The Three Dahlias", author: "Katy Watson" },
+  { id: 9, bookTitle: "The Murders at Fleat House", author: "Lucinda Rey" },
+  { id: 10, bookTitle: "The Appeal", author: "Janice Hallett" },
+  { id: 11, bookTitle: "Witch & Wizard", author: "James Patterson" },
+  { id: 12, bookTitle: "The Gift", author: "James Patterson" },
+  { id: 13, bookTitle: "The Fire", author: "James Patterson" },
+  { id: 14, bookTitle: "The Kiss", author: "James Patterson" },
+  { id: 15, bookTitle: "The Lost", author: "James Patterson" },
+  { id: 16, bookTitle: "The Maid", author: "Nita Prose Nita Prose" },
+  { id: 17, bookTitle: "A Meditation on Murder", author: "Robert Thorogood" },
+  {
+    id: 18,
+    bookTitle: "The Killing of Polly Carter",
+    author: "Robert Thorogood",
+  },
+  { id: 19, bookTitle: "Death Knocks Twice", author: "Robert Thorogood" },
+  { id: 20, bookTitle: "Murder in the Caribbean", author: "Robert Thorogood" },
+];
+
+export default data2023;
