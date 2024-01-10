1141884820d60eb3e1880d3c7adbd856151ad232
Preet Shah <shahpreetk@gmail.com>
2024-01-10 23:43:20 +0000

    books read in 2024

---
+++ blog/data2024.js
@@ -0,0 +1,6 @@
+const data2024 = [
+  { id: 1, bookTitle: "Genghis Khan and the Making of the Modern World", author: "Jack Weatherford" },
+  { id: 2, bookTitle: "Oliver Twist", author: "Charles Dickens" },
+];
+
+export default data2024;
