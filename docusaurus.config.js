9bec56a022eb626afc66965a1ecce4bd9c1a2519
Preet Shah <shahpreetk@gmail.com>
2024-02-10 23:54:38 +0000

    updated sitemap config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
         },
         sitemap: {
           changefreq: "weekly",
+          priority: 0.80,
           ignorePatterns: ["/tags/**"],
           filename: "sitemap.xml",
         },
