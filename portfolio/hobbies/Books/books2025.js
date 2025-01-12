bcfc7ef6a2e58206abae333a76130df7bd946f38
Preet Shah <shahpreetk@gmail.com>
2025-01-12 15:34:43 +0000

    feat: added books I read in 2025

---
+++ portfolio/hobbies/Books/books2025.js
@@ -0,0 +1,29 @@
+const books2025 = [
+  {
+    id: 1,
+    bookTitle: "The Binding Chronicles - Tethered",
+    author: "Elayna R. Gallea",
+  },
+  {
+    id: 2,
+    bookTitle: "The Binding Chronicles - Tormented",
+    author: "Elayna R. Gallea",
+  },
+  {
+    id: 3,
+    bookTitle: "The Binding Chronicles - Treasured",
+    author: "Elayna R. Gallea",
+  },
+  {
+    id: 4,
+    bookTitle: "Throne of Glass",
+    author: "Sarah J. Maas",
+  },
+  {
+    id: 5,
+    bookTitle: "Crown of Midnight",
+    author: "Sarah J. Maas",
+  },
+];
+
+export default books2025;
